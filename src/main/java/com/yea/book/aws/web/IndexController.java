b64f6d20a63d0b2871cc178ef3381382a86b7de5
yeahyung <yhb77888@naver.com>
2020-08-05 23:56:34 +0900

    2020-08-05 p224 OAuth 소셜 로그인, 테스트 실패 패스

--- src/main/java/com/yea/book/aws/web/IndexController.java
+++ src/main/java/com/yea/book/aws/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.yea.book.aws.web;
 
+import com.yea.book.aws.config.auth.LoginUser;
 import com.yea.book.aws.config.auth.dto.SessionUser;
 import com.yea.book.aws.service.posts.PostsService;
 import com.yea.book.aws.web.dto.PostsResponseDto;
@@ -19,10 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser)httpSession.getAttribute("user");
 
         if(user != null){
             model.addAttribute("userName", user.getName());
